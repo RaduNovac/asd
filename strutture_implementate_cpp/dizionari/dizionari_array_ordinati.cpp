08360e94ed31701b36933d81911948296a04a8ff
Novac Radu <obs@live.it>
2024-11-07 15:38:24 +0100

    Finally fixed?
    Maybe

--- strutture_implementate_cpp/dizionari/dizionari_array_ordinati.cpp
+++ strutture_implementate_cpp/dizionari/dizionari_array_ordinati.cpp
@@ -12,129 +12,128 @@ using namespace std;
 
 struct Coppia {
     int key;
-    string valore;
+    string info;
 };
 
 typedef Coppia Coppia;
 
 class Dizionario {
     public:
-        Coppia* S;
-        int numeroElementi;
+        Coppia* A;
+        int length;
 
         Dizionario(){
-            numeroElementi = 0;
-            S = (Coppia*) malloc(numeroElementi * sizeof(Coppia));
+            length = 0;
+            A = (Coppia*) malloc(length * sizeof(Coppia));
         }
         //Stampa
         void stampa(){
-            cout << "\nIl vettore S:\n";
+            cout << "\nIl vettore A:\n";
 
-            for(int i = 0; i < numeroElementi; i++){
-                cout << "S["<< i <<"]: " << S[i].key << ", " << S[i].valore << endl;
+            for(int i = 0; i < length; i++){
+                cout << "A["<< i <<"]: key = " << A[i].key << ", info = " << A[i].info << endl;
             }
         }
 
         //Ricerca
-        void search(int key){
-            if(search_index(key) != -1){
-                cout << "Trovato!\n";
+        void search(int k){
+            int i = search_index(k, 0, length);
+
+            if(i != -1){
+                cout << "Trovato! " << A[i].info << endl;
             }
             else{
                 cout << "Elemento non presente!\n";
             }
         }
 
-        int search_index(int key){
-            return search_index_aux(key, 0, numeroElementi - 1);
-        }
-
-        int search_index_aux(int key, int left, int right){
-            //Se l'indice sx è > di quello dx, cioè non ho un range valido
-            if(left > right) {
+        int search_index(int k, int l, int r){
+            if(r < l){
                 return -1;
             }
-            //Trovo l'indice di mezzo
-            int mid = left + (right - left) / 2;
-
-            //Se ho trovato l'elemento
-            if(S[mid].key == key){
-                return mid;
-            } 
-            //Se il mio valore è maggiore, cerco nella parte dx
-            else if(S[mid].key < key){
-                return search_index_aux(key, mid + 1, right);
-            }
-            //Altrimenti è <= e cerco nella parte sx
             else{
-                return search_index_aux(key, left, mid - 1);
+                int mid = (l + r) / 2;
+
+                if(A[mid].key == k){
+                    return mid;
+                }
+                else{
+                    if(A[mid].key > k){
+                        return search_index(k, l, mid - 1);
+                    }
+            //Altrimenti è <= e cerco nella parte sx
+                    else{
+                        return search_index(k, mid + 1, r);
+                    }
+                }
             }
         }
         //Inserimento
-        void insert(int key, string valore) {
-            //Se la chiave che voglio inserire c'è già la aggiorno, altrimenti la aggiungo trovando la posizione, ingrandendo il vettore di 1 posizione e shiftando tutto di 1 a dx
-
-            int posizione = search_index(key);
-
-            if(posizione != -1){//Se è già presente la aggiorno
-                cout << "Già presente! Provvedo ad aggiornare il valore...\n";
-
-                S[posizione].valore = valore;
+        void insert(int k, string info) {
+            int i = 0;
+            //trovo l'indice del record avente chiave k
+            while(i <= length && A[i].key < k){
+                i = i + 1;
             }
-            else{//Altrimenti inserisco una nuova coppia
+            //Se c'è già, aggiorno il campoinfo
+            if(i <= length && A[i].key == k){
+                A[i].info = info;
+            }
+            //Altrimenti rialloco il vettore e shifto tutti gli elementi a dx di 1
+            else{
                 int pos = 0;
 
                 //Creo la nuova coppia con i valori
-                Coppia nuovaCoppia = {key, valore};
+                Coppia nuovaCoppia = {k, info};
                 
                 //Trovo la posizione dove inserirlo
-                while(pos < numeroElementi && S[pos].key < key) {
+                while(pos < length && A[pos].key < k) {
                     pos++;
                 }
                 //Creo un nuovo vettore di dimensione + 1
 
-                Coppia* TMP = (Coppia*)realloc(S,(numeroElementi + 1) * sizeof(Coppia));
+                Coppia* TMP = (Coppia*)realloc(A,(length + 1) * sizeof(Coppia));
 
                 //Se l'allocazione del nuovo vettore è andata a buon fine
                 if(TMP != NULL){
                     //Aumento numeroElementi di 1
-                    numeroElementi++;
+                    length++;
 
                     //Ricopio i valori dentro al nuovo vettore
 
-                    S = TMP;
+                    A = TMP;
 
-                    S[numeroElementi - 1] = S[numeroElementi - 2];//Riempio l'ultima coppia
+                    A[length - 1] = A[length - 2];//Riempio l'ultima coppia
 
                     //Shifto dalla posizione dove devo inserire a dx di 1 tutti gli elementi
-                    for(int i = numeroElementi - 1; i > pos; i--){
-                        S[i] = S[i - 1];
+                    for(int i = length - 1; i > pos; i--){
+                        A[i] = A[i - 1];
                     }
 
                     //Inserisco nella posizione corretta
 
-                    S[pos] = nuovaCoppia;
+                    A[pos] = nuovaCoppia;
                 }
             }
         }
         //Cancellazione
-        void cancel(int key){
+        void cancel(int k){
             //Cerco l'indice della chiave key
-            int index = search_index(key);
+            int index = search_index(k, 0, length);
             
             //Shifto a sx di 1 gli elementi > di key
-            for(int i = index; i < numeroElementi - 1; i++){
-                S[i].key = S[i + 1].key;
-                S[i].valore = S[i + 1].valore;
+            for(int i = index; i < length - 1; i++){
+                A[i].key = A[i + 1].key;
+                A[i].info = A[i + 1].info;
             }
             //Rialloco lo spazio a numeroElementi - 1
-            Coppia* TMP = (Coppia*)realloc(S,(numeroElementi - 1) * sizeof(Coppia));
+            Coppia* TMP = (Coppia*)realloc(A,(length - 1) * sizeof(Coppia));
 
             //Se il realloc va a buon fine
             if(TMP != NULL){
-                numeroElementi--;
+                length--;
 
-                S = TMP;
+                A = TMP;
             }
         }
 };
@@ -142,25 +141,23 @@ class Dizionario {
 int main() {
     Dizionario dizionario;
 
-    if(!dizionario.S){
+    if(!dizionario.A){
         cout << "Assegnazione vettore S NON andata a buon fine!\n";
         exit(1);
     }
-
+    
+    dizionario.insert(5,"Giacomo");
+    dizionario.insert(2,"Alberto");
+    dizionario.insert(3,"Paolo");
     dizionario.insert(1,"Radu");
-    dizionario.insert(6,"Andrea");
-    dizionario.insert(3,"Piero");
-    dizionario.insert(4,"Giulio");
-    dizionario.insert(5,"Lorenzo");
-    dizionario.insert(2,"Alvise");
-    dizionario.insert(8,"Francesco");
-    dizionario.insert(9,"Pino");
-    dizionario.insert(7,"Alessandro");
-    
-    dizionario.stampa();
-    /*
-    dizionario.cancel(4);
-    
+    dizionario.insert(4,"Matteo");
+    dizionario.insert(6,"Lorenzo");
+    dizionario.insert(9,"Aldo");
+    dizionario.insert(8,"Giovanni");
+    dizionario.insert(7,"Ermanno");
+
+    dizionario.cancel(6);
+
     dizionario.stampa();
     return 0;
 }
