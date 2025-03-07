1d8118438f09731107e6be64d7f26f53480d05c3
Radu <obs@live.it>
2025-03-07 16:29:34 +0100

    updated output and structure

--- esercizi/stampa_livello/main.cpp
+++ esercizi/stampa_livello/main.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
+/*
+Sia T un albero binario. Progettare un algoritmo che, presi in ingresso la radice
+di T e un intero k che risulti essere maggiore di 0, stampi le chiavi contenuti 
+nei nodi di T al livello k, procedendo da sinistra verso destra.
+*/
+
 struct node{
     int key;
     node* left;
     node* right;
     node* p;
 
-    node(int k, node* padre = nullptr, node* sx = nullptr, node* dx = nullptr)
-            : key{k}, p{padre}, left{sx}, right{dx} {}
+    node(int k, node* sx = nullptr, node* dx = nullptr)
+            : key{k}, left{sx}, right{dx} {}
 }; typedef node* pnode;
-
+//Do per scontato che k sia > 0 e < num_livelli albero
 void stampa_livello(pnode u, int k){
     if(u != nullptr){//Se l'albero non è vuoto
-        if(k == 0){
-            cout << u -> key;
+        if(k == 0){//Se è il livello che voglio
+            cout << u -> key << "; ";
         }
-        else{
+        else{//Altrimenti scendo di 1 livello
             stampa_livello(u -> left, k-1);
             stampa_livello(u -> right, k-1);
         }
@@ -26,14 +32,13 @@ void stampa_livello(pnode u, int k){
 }
 
 int main() {
-    //Albero 3_bilanciato
     node nodo_1{1};
-    node nodo_2{2,&nodo_1};
-    node nodo_3{3,&nodo_1};
-    node nodo_4{4,&nodo_2};
-    node nodo_5{5,&nodo_3};
-    node nodo_6{6,&nodo_3};
-    node nodo_7{7,&nodo_4};
+    node nodo_2{2};
+    node nodo_3{3};
+    node nodo_4{4};
+    node nodo_5{5};
+    node nodo_6{6};
+    node nodo_7{7};
 
     nodo_1.left = &nodo_2;
     nodo_1.right = &nodo_3;
