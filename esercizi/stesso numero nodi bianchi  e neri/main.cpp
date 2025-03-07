605c5dbe40819c7153347e00cec49947d05c75d5
Radu <obs@live.it>
2025-03-07 17:58:45 +0100

    updated, added professor version

--- esercizi/stesso numero nodi bianchi  e neri/main.cpp
+++ esercizi/stesso numero nodi bianchi  e neri/main.cpp
@@ -1,6 +1,9 @@
 /*
- * Funzione efficiente che calcoli il n° di nodi avente lo stesso n° di discendenti bianchi e neri.
- * */
+Dato un albero binario i cui nodi sono colorati di bianco e nero, scrivere una
+funzione efficiente che calcoli il numero di nodi aventi lo stesso numero di 
+discendenti bianchi e neri. Ricordiamo che un nodo è discendente di sé stesso.
+Analizzare la complessità della funzione.
+*/
 #include <iostream>
 
 using namespace std;
@@ -11,58 +14,90 @@ struct node{
     node* right;
     node* p;
 
-    node(int k, node* padre = nullptr, node* sx = nullptr, node* dx = nullptr)
-        : is_black{k}, p{padre}, left{sx}, right{dx} {}
+    node(int k, node* sx = nullptr, node* dx = nullptr)
+        : is_black{k}, left{sx}, right{dx} {}
 }; typedef  node *pnode;
-
+//Versione mia
 int discendenti_bianchi_neri_aux(pnode u, int& nodi){
-    int sum = 0 ;
-    if(u->left==nullptr&&u->right==nullptr){
-        return u->is_black ? 1 : -1 ;
+    int sum = 0;
+    /*
+    Se u è una foglia, coltrolla se è bianca o nera e ritorna il valore 
+    per la somma in modo che aggiunga 1 se nera, altrimenti toglie 1 se bianca
+    */
+    if(u -> left == nullptr && u -> right == nullptr){
+        return u -> is_black ? 1 : -1;//Se u è nero ritorna 1, altrimenti -1
     }
     else{
-        if(u->left!=nullptr){
-            sum += discendenti_bianchi_neri_aux(u->left, nodi);
+        if(u -> left != nullptr){//Se ho un figlio sx
+            sum += discendenti_bianchi_neri_aux(u -> left, nodi);
         }
-        if(u->right!=nullptr){
-            sum += discendenti_bianchi_neri_aux(u->right, nodi);
+        if(u -> right != nullptr){//Se ho un figlio dx
+            sum += discendenti_bianchi_neri_aux(u -> right, nodi);
         }
-        if(sum==0){
-            nodi += 1 ;
+        /*
+        Se la somma è 0, vuol dire che il sottoalbero radicato in u ha lo
+        stesso numero di discendenti bianchi e neri
+        */
+        if(sum == 0){
+            nodi += 1;
         }
-        return sum + u->is_black ? 1 : -1 ;
+        return sum + u -> is_black ? 1 : -1;
     }
 }
 int discendenti_bianchi_neri(pnode u){
-    if(u==nullptr||(u->left==nullptr&&u->right==nullptr)){
+    //Se u è nullo oppure foglia
+    if(u == nullptr || (u -> left == nullptr && u -> right == nullptr)){
         return 0 ;
     }
-    else{
+    else{//Se invece è un nodo interno
         int nodi = 0;
         discendenti_bianchi_neri_aux(u, nodi);
         return nodi;
     }
 }
+//Versione prof
+void sameColorAux (pnode u, int& count, int& bianchi, int& neri){
+    if(u){
+        int bsx = 0, nsx = 0, bdx = 0 , ndx = 0;
+
+        sameColorAux (u -> left , count , bsx , nsx );
+        sameColorAux (u -> right , count , bdx , ndx );
+
+        bianchi = bsx + bdx + (u -> is_black == 0);
+        neri = nsx + ndx + (u -> is_black == 1);
+        cout << "bianchi: " << bianchi << ";neri: " << neri << endl;
+        if (bianchi == neri){
+            count++;
+        }
+    }
+}
+int discSameColor(pnode u){
+    int count = 0, bianchi = 0 , neri = 0;
+
+    sameColorAux (u , count , bianchi , neri );
+
+    return count;
+}
 
 int main() {
     int x = 0;
 
-    //Padri
+    //Padri, 1 = true = nero, 0 = false = bianco
     node nodo_1{0};
-    node nodo_2{1,&nodo_1};
-    node nodo_3{0,&nodo_1};
-    node nodo_4{1,&nodo_2};
-    node nodo_5{1,&nodo_2};
-    node nodo_6{1,&nodo_3};
-    node nodo_7{0,&nodo_3};
-    node nodo_8{0,&nodo_4};
-    node nodo_9{1,&nodo_4};
-    node nodo_10{0,&nodo_5};
-    node nodo_11{0,&nodo_5};
-    node nodo_12{0,&nodo_6};
-    node nodo_13{1,&nodo_6};
-    node nodo_14{0,&nodo_7};
-    node nodo_15{1,&nodo_7};
+    node nodo_2{1};
+    node nodo_3{0};
+    node nodo_4{1};
+    node nodo_5{1};
+    node nodo_6{1};
+    node nodo_7{0};
+    node nodo_8{0};
+    node nodo_9{1};
+    node nodo_10{0};
+    node nodo_11{0};
+    node nodo_12{0};
+    node nodo_13{1};
+    node nodo_14{0};
+    node nodo_15{1};
     //Figli
     nodo_1.left = &nodo_2;
     nodo_1.right = &nodo_3;
@@ -79,7 +114,8 @@ int main() {
     nodo_7.left = &nodo_14;
     nodo_7.right = &nodo_15;
 
-    x = discendenti_bianchi_neri(&nodo_1);
+    //x = discendenti_bianchi_neri(&nodo_1);
+    x = discSameColor(&nodo_1);
 
     cout << x << " nodi con numero di discendenti bianchi e neri uguali." << endl;
 
